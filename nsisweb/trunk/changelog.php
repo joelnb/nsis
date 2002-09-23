f2bb8baabd5e3c088efef7ec57404291287d24c3
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2002-09-23 16:32:20 +0000

    added eccles
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@44 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/changelog.php
+++ nsisweb/trunk/changelog.php
@@ -4,7 +4,8 @@
 		array("rainwater","Robert Rainwater"),
 		array("justin1014","Justin Frankel"),
 		array("kichik","Amir Szekely"),
-		array("joostverburg","Joost Verburg")
+		array("joostverburg","Joost Verburg"),
+		array("eccles","Dave Laundon")
 	);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
