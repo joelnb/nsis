84cf940b82d51731d8176b2158f443d3d498c272
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2002-09-23 16:30:46 +0000

    Updated changelog
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@43 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/changelog.php
+++ nsisweb/trunk/changelog.php
@@ -1,4 +1,12 @@
 <? include("include/global.php"); ?>
+<?
+	$_AUTHORS = array(
+		array("rainwater","Robert Rainwater"),
+		array("justin1014","Justin Frankel"),
+		array("kichik","Amir Szekely"),
+		array("joostverburg","Joost Verburg")
+	);
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
 <html> 
 <head> 
@@ -6,7 +14,18 @@
 </head> 
 <body> 
 <? IncludePageHeader("NSIS Changelog",true); ?>
-<pre class="changelog"><?php @readfile('/home/groups/n/ns/nsis/htdocs/nightly/changelog.txt') ?></pre> 
+<pre class="changelog">
+<?php 
+$contenta = @file('/home/groups/n/ns/nsis/htdocs/nightly/changelog.txt');
+if ($contenta) {
+	$content = implode ('',$contenta);
+	for ($i = 0; $i < count($_AUTHORS); $i++) {
+		$content = str_replace($_AUTHORS[$i][0],$_AUTHORS[$i][1],$content);
+	}
+	echo $content;
+}
+?>
+</pre> 
 <? IncludePageFooter(); ?>
 </body> 
 </html> 
