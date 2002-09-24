2f7368dfb39531e0c9544d434f488087a89c2489
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2002-09-24 11:57:32 +0000

    no str_replace
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@56 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/changelog.php
+++ nsisweb/trunk/changelog.php
@@ -1,13 +1,4 @@
 <? include("include/global.php"); ?>
-<?
-	$_AUTHORS = array(
-		array("rainwater","Robert Rainwater"),
-		array("justin1014","Justin Frankel"),
-		array("kichik","Amir Szekely"),
-		array("joostverburg","Joost Verburg"),
-		array("eccles","Dave Laundon")
-	);
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
 <html> 
 <head> 
@@ -19,10 +10,6 @@
 <?php 
 $contenta = @file('/home/groups/n/ns/nsis/htdocs/nightly/changelog.txt');
 if ($contenta) {
-	$content = implode ('',$contenta);
-	for ($i = 0; $i < count($_AUTHORS); $i++) {
-		$content = str_replace($_AUTHORS[$i][0],$_AUTHORS[$i][1]." (".$_AUTHORS[$i][0].")",$content);
-	}
 	echo $content;
 }
 ?>
