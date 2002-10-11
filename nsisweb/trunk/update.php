382a8e4fdfceaa4f7b973565ff7f021b1ae37927
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2002-10-11 13:15:31 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@78 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ nsisweb/trunk/update.php
@@ -0,0 +1,42 @@
+<?
+
+$latest = "2.0b0";
+
+?>
+<? include("include/global.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<html> 
+<head> 
+<base href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/nsis/NSIS/">
+<? IncludeHTMLHeader("Changelog"); ?>
+</head> 
+<body> 
+<? IncludePageHeader("NSIS Changelog",true); ?>
+<?php 
+if ($version) {
+	$update = false;
+	$version = str_replace("v","",$version);
+	$pos = strpos($version,"b");
+	if (!$pos) $pos = strpos($version,"a");
+	if ($pos) {
+        $version = substr($version,0,$pos);
+        if ($latest>=$version) $update = true;
+	}
+	else {
+        if ($latest>$version) $update = true;
+	}
+	if ($update) {
+		"There is a new version of NSIS avaialable.  You may download it <a href=\"http://sourceforge.net/project/showfiles.php?group_id=22049\">here</a>.";
+	}
+	else {
+		"You have the latest version of NSIS installed.";
+	}
+}
+else {
+	print "Version parameter not found.";
+}
+
+?>
+<? IncludePageFooter(); ?>
+</body> 
+</html> 
