e9c1b0756fced7d316d85616bcf43375e63974b6
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2002-10-17 00:50:41 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@92 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/update.php
+++ nsisweb/trunk/update.php
@@ -12,7 +12,16 @@ if ($version) {
         $series = substr($version,0,$pos);
 		$revision = substr($version,$pos+1);
         if ($latest>=$series) print "1|".$latest;
-		else if (!strcmp($betaseries,$series)&&$betaversion>$revision) print "1|".$betaseries.$betaversion;
+		else if (!strcmp($betaseries,$series)) {
+			if (!strncmp($betaversion,$revision,1)) {
+				if (substr($betaversion,1)>substr($revesion)) print "2|".$betaseries.$betaversion;
+				else print "0";
+			}
+			else if (!strncmp($betaversion,"b",1)&&!strncmp($series,"a",1)) {
+				print "2|".$betaseries.$betaversion;
+			}
+			else print "0";
+		}&&$betaversion>$revision) print "2|".$betaseries.$betaversion;
 		else print "0";
 	}
 	else {
