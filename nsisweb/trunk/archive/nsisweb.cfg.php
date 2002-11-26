cc8447a034d2d2611c01466bf5f8bc9f7c8303f9
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-11-26 20:45:00 +0000

    Now refers to config file outside the web server root.
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@104 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/nsisweb.cfg.php
+++ nsisweb/trunk/archive/nsisweb.cfg.php
@@ -1,13 +1,4 @@
 <?
-/* This is an example file only, you should configure it correctly */
-define('NSISWEB_WWWROOT','http://my.web.server');
-define('NSISWEB_WWWSUBDIR','/archive');
-define('NSISWEB_FILEROOT',$_SERVER['DOCUMENT_ROOT']);
-define('NSISWEB_FILESUBDIR','/archive');
-define('NSISWEB_MYSQL_HOST','127.0.0.1');
-define('NSISWEB_MYSQL_USER','user');
-define('NSISWEB_MYSQL_PASSWORD','pass');
-define('NSISWEB_OWNDB',FALSE);
-define('NSISWEB_DB','nsisweb');
-define('NSISWEB_MAXSINGLEUPLOAD',10485760);
+/* keep the password file out of the web server root */
+include_once('/home/groups/n/ns/nsis/nsisweb.cfg.php');
 ?>
