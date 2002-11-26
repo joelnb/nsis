3bab40f8dfc261c1f56fb376b02d4510123ca819
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-11-26 20:01:43 +0000

    Check-in of the copy of the code currently working on nsis.sf.net/archive.
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@103 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/nsisweb.cfg.php
+++ nsisweb/trunk/archive/nsisweb.cfg.php
@@ -1,12 +1,13 @@
 <?
-define('NSISWEB_WWWROOT','http://127.0.0.1:8080');
-define('NSISWEB_WWWSUBDIR','/nsisweb');
+/* This is an example file only, you should configure it correctly */
+define('NSISWEB_WWWROOT','http://my.web.server');
+define('NSISWEB_WWWSUBDIR','/archive');
 define('NSISWEB_FILEROOT',$_SERVER['DOCUMENT_ROOT']);
-define('NSISWEB_FILESUBDIR','/nsisweb');
+define('NSISWEB_FILESUBDIR','/archive');
 define('NSISWEB_MYSQL_HOST','127.0.0.1');
-define('NSISWEB_MYSQL_USER','theuser');
-define('NSISWEB_MYSQL_PASSWORD','whatever');
+define('NSISWEB_MYSQL_USER','user');
+define('NSISWEB_MYSQL_PASSWORD','pass');
 define('NSISWEB_OWNDB',FALSE);
-define('NSISWEB_DB','nsis');
+define('NSISWEB_DB','nsisweb');
 define('NSISWEB_MAXSINGLEUPLOAD',10485760);
 ?>
