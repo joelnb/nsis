3dfff96efe144f82dce9e0e2d87d405b059a0dc6
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-11-28 23:25:23 +0000

    templates now live in their own directory
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@108 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ nsisweb/trunk/archive/templates/dir_page.skel.php
@@ -0,0 +1,30 @@
+	<font style="font-family: verdana; font-size: 20pt; color: #000000;">$title</font><br>
+	<p><i>$source</i></p>
+<?
+	/* This file is a template. It is included by the NSISWeb core when it needs
+	   to display a page that is a container for other pages. The properties of
+	   the so called "directory page" and the actual content are
+	   variables available to us. We are expected to use these in this file
+	   thereby "filling" the template.
+	   
+	   We receive:-
+			 + $title   : directory/container/section title
+			 + $desc    : description of the section
+			 + $author  : original author of the section
+			 + $date    : original creation date of the section
+			 + $pageid  : the NSISWeb internal id of the section
+			 + $nsisweb : the NSISWeb core object instance
+			 + $items   : an array of contained pages
+			   -> $pageid
+			   -> $title
+*/
+?>
+<font style="font-family: verdana; font-size: 20pt; color: #000000;"><?= $title ?></font><br>
+<p><i><?= $desc ?></i></p>
+<p>
+	<ul>
+		<? foreach($items as $item) { ?>
+		<li><a href="<?= $nsisweb->wwwroot.'/nsisweb.php?page='.$item['pageid'] ?>"><?= $item['title'] ?></a>
+		<? } ?>
+	</ul>
+</p>
---
+++ nsisweb/trunk/archive/templates/footer.skel.php
@@ -0,0 +1,22 @@
+<?
+/* This is a template file, it is invoked prior to dumping out page content.
+   It should be used to create title banners and to position/surround/etc the
+   page content to come.
+   
+   Defined already are:
+     + $nsisweb
+     + $page_info (array)
+     + $title
+*/
+?>
+        						<p>&nbsp;<br>&nbsp;<br><a href="http://sourceforge.net"><img class="image" src="http://sourceforge.net/sflogo.php?group_id=22049&amp;type=1" alt="SourceForge Logo"></a> 
+									</div>
+								</td> 
+							</tr> 
+						</table> 
+					</td>
+				</tr> 
+			</table> 
+		</div> 
+	</body> 
+</html>
