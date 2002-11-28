852e0658d6fc38aa943e8059211962841f5640aa
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-11-28 23:30:18 +0000

    moved into template dir
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@110 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ nsisweb/trunk/archive/templates/404.php
@@ -0,0 +1,20 @@
+<?
+include_once("engine/nsisweb.pkg.php");
+$nsisweb->start_page('Page Not Found');
+?>
+<center>
+	<table width="75%" border="0" cellpadding="0" cellspacing="0">
+	  <tr>
+	    <td align="left">
+	      <img border="0" align="top" src="<?= $nsisweb->wwwroot.'/images/exclamation.bmp' ?>">
+	    </td>
+	    <td align="left" valign="middle" style="font-size:10pt;margin:12px;">
+	      The page you were looking for, is not currently available, or you may have
+	      mis-typed the address. Please check and try again.<br>
+	      <br>
+	      Requested Page: <?= $_GET['page'] ?><br>
+	    </td>
+	  </tr>
+	</table>
+</center>
+<? $nsisweb->end_page(); ?>
