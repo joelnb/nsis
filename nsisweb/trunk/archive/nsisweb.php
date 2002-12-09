5a2ff837c7ff31f83c084dd67a94785bca9d2c73
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2002-12-09 16:08:56 +0000

    No need to promote an account creation if the user is already logged in
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@180 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/nsisweb.php
+++ nsisweb/trunk/archive/nsisweb.php
@@ -34,7 +34,12 @@ NSIS users for NSIS users.</p>
   <li><b><a href="<?= $nsisweb->get_page_url('popular') ?>">Most Popular</a></b>
     <ul>See which pages are visited most and which files are downloaded most frequently</ul>
   </li>
+<?
+if ($nsisweb->get_session()->is_anonymous()) {
   <li><b><a href="<?= $nsisweb->get_page_url('createaccount') ?>">Your Account</a></b>
     <ul>Get your free account with us and get the credit for your contributions</ul>
   </li>
-<? $nsisweb->end_page(); ?>
+<?
+}
+  
+$nsisweb->end_page(); ?>
