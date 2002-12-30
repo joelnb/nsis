05e1bc96091b5cd555ebdda3b7db94cc861d49e9
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-12-30 19:54:33 +0000

    Limit ranked pages to a maximum of 25 and exclude directory pages.
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@270 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/popular.php
+++ nsisweb/trunk/archive/popular.php
@@ -4,7 +4,7 @@ include_once(dirname(__FILE__)."/engine/nsiswebstorage.pkg.php");
 $nsisweb->start_page('Most Popular');
 ?>
 <font style="font-family: verdana; font-size: 20pt; color: #000000;">Most Popular</font>
-<p>The most viewed pages are:</p>
+<p>The top 25 most popular pages (excluding directory pages) by page views are:</p>
 <br>
 <center>
   <table width="90%" cellpadding="2" cellspacing="0" border="0">
@@ -14,13 +14,13 @@ $nsisweb->start_page('Most Popular');
       <th align="center"><b>&nbsp;Views&nbsp;</b></th>
     </tr>
 <?
-$result = $nsisweb->query("select title,views,pageid from nsisweb_pages where views>0 order by views desc");
+$result = $nsisweb->query("select title,views,pageid from nsisweb_pages where views>0 and type<>'.PAGETYPE_DIRECTORY.' order by views desc");
 if($result) {
 	$count = $nsisweb->how_many_results($result);
 	$rank  = 1;
 	if($count > 0) {
 		$i = 0;
-		while(is_array($record = $nsisweb->get_result_array($result))) {
+		while($rank <= 25 && is_array($record = $nsisweb->get_result_array($result))) {
 			$title = '<a href="viewpage.php?pageid='.$record['pageid'].'" target="_blank">'.$record['title']."</a>\n";
 			$views = (int)$record['views'];
 			
@@ -31,7 +31,7 @@ if($result) {
 				$i = 0;
 				$bgcolour = '#ddddff';
 			}
-			
+
 			print <<<ENDOFHTML
 			<tr style="background-color:$bgcolour;">
 			  <td align="left" valign="middle" style="font-family:verdana;font-size:10pt;">
