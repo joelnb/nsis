2546cfb5e01f68363babbb23253f14eec0cc9bc2
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-12-30 15:38:24 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@266 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ nsisweb/trunk/archive/viewpage.php
@@ -0,0 +1,12 @@
+<?
+include_once(dirname(__FILE__)."/engine/nsisweb.pkg.php");
+
+if($page = new NsisWebPage($_GET['pageid'],FETCH_CONTENT_PP)) {
+	$instance = new NsisWebInstance($page);
+	$instance->show();
+	exit;
+}
+
+$url = $nsisweb->get_base_url().'/templates/404.php?page='.urlencode($_GET['pageid']);
+header("Location: $url");
+?>
