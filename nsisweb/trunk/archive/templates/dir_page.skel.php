989b3504aa40978cf4b44dfc53ce2469bb89293d
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2002-12-31 02:06:50 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@276 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/templates/dir_page.skel.php
+++ nsisweb/trunk/archive/templates/dir_page.skel.php
@@ -1,19 +1,27 @@
 <?
-$insert_link = '<a href="picklist.php?instanceid='.$this->get_instanceid().'">insert page</a>';
-$delete_link = '<a href="delete.php?instanceid='.$this->get_instanceid.'">delete page</a>';
+function get_rollover($img)
+{
+	return 'onMouseOver="'.$img.'.src=\'images/'.$img.'2.png\'" onMouseOut="'.$img.'.src=\'images/'.$img.'.png\'"';
+}
+function get_image($img)
+{
+	return '<img src="images/'.$img.'.png" name="'.$img.'" width="20" height="20" border="0" alt="'.$img.'">';
+}
+
+$insert_link = '<a href="picklist.php?instanceid='.$this->get_instanceid().'" '.get_rollover('paste').'>'.get_image('paste').'</a>';
 if($this->get_parentid() == 0) {
-  $edit_link   = '<font color="#aaaaaa">edit</a>';
-  $delete_link = '<font color="#aaaaaa">delete</font>';
+  $edit_link   = get_image('edit');
+  $delete_link = get_image('cut');
 } else {
-  $edit_link   = '<a href="edit.php?instanceid='.$this->get_instanceid().'">edit</a>';
-  $delete_link = '<a href="delete.php?instanceid='.$this->get_instanceid().'">delete</a>';
+  $edit_link   = '<a href="edit.php?instanceid='.$this->get_instanceid().'" '.get_rollover('edit').'>'.get_image('edit').'</a>';
+  $delete_link = '<a href="delete.php?instanceid='.$this->get_instanceid().'" '.get_rollover('cut').'>'.get_image('cut').'</a>';
 }
 if($this->get_instanceid() == 0) {
-	$up_link   = '<a href="'.$nsisweb->get_home_url().'">up</a>';
-	$pick_link = '<font color="#aaaaaa">pick</a>';
+	$up_link   = '<a href="'.$nsisweb->get_home_url().'" '.get_rollover('up').'>'.get_image('up').'</a>';
+	$pick_link = get_image('copy');
 } else {
-	$up_link   = '<a href="'.$nsisweb->get_page_url($this->get_parentid()).'">up</a>';
-	$pick_link = '<a href="picklist.php?action=pick&instanceid='.$this->get_instanceid().'">pick</a>';
+	$up_link   = '<a href="'.$nsisweb->get_page_url($this->get_parentid()).'" '.get_rollover('up').'>'.get_image('up').'</a>';
+	$pick_link = '<a href="picklist.php?action=pick&instanceid='.$this->get_instanceid().'" '.get_rollover('copy').'>'.get_image('copy').'</a>';
 }
 
 if($page = $this->get_page()) {
@@ -21,7 +29,7 @@ if($page = $this->get_page()) {
 <!-- dir_page.skel.php: begin -->
 <table style="font-family:verdana;font-size:8pt;color:#000000;" width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
 <td align="left"><font style="font-size:20pt;"><?= $page->get_title() ?></font></td>
-<td align="right" valign="top"><?= $edit_link ?> | <?= $pick_link ?> | <?= $delete_link ?> | <?= $insert_link ?> | <?= $up_link ?></td>
+<td align="right" valign="top"><?= $edit_link ?> | <?= $delete_link ?> | <?= $pick_link ?> | <?= $insert_link ?> | <?= $up_link ?></td>
 </tr></table>
 <p><i><?= $page->get_pp_content() ?></i></p>
 <p>
