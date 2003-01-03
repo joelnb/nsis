843bfeeb592d53daf77f005d298acb69d7019f68
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2003-01-03 12:57:39 +0000

    We don't want everyone regenerating all of the pages, only admins
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@341 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/regenerate.php
+++ nsisweb/trunk/archive/regenerate.php
@@ -4,23 +4,30 @@ include_once(dirname(__FILE__)."/engine/nsisweb.pkg.php");
 $nsisweb->start_page('Regenerating Content');
 echo '<font style="font-family:verdana;font-size:20pt;color:#000000;">Regenerate Content</font>';
 
-$result = $nsisweb->query("select pageid from nsisweb_pages");
-if($result && $nsisweb->how_many_results($result)>0) {
-	while($record = $nsisweb->get_result_array($result)) {
-		$pages[] = $record['pageid'];
-	}
-	print '<p>Found '.count($pages).' to regenerate.<br><br></p><ul>';
-	foreach($pages as $pageid) {
-		$page = find_pageid($pageid);
-		if($page) {
-		  print '<li>Regenerating '.$page->get_title();
-			$page->save($page->get_title(),$page->get_content(),$page->get_flags(),TRUE);
-			print "</li>\n";
-		}
-	}
-	print '</ul><p>Done.</p>';
+$session = $nsisweb->get_session();
+$user    = find_userid($session->user_id);
+
+if(!$user->is_admin()) {
+	echo '<p>This page is for users with admin rights only.</p>';
 } else {
-	print '<p>There are no pages in the archive to operate on.</p>';
+	$result = $nsisweb->query("select pageid from nsisweb_pages");
+	if($result && $nsisweb->how_many_results($result)>0) {
+		while($record = $nsisweb->get_result_array($result)) {
+			$pages[] = $record['pageid'];
+		}
+		print '<p>Found '.count($pages).' to regenerate.<br><br></p><ul>';
+		foreach($pages as $pageid) {
+			$page = find_pageid($pageid);
+			if($page) {
+			  print '<li>Regenerating '.$page->get_title();
+				$page->save($page->get_title(),$page->get_content(),$page->get_flags(),TRUE);
+				print "</li>\n";
+			}
+		}
+		print '</ul><p>Done.</p>';
+	} else {
+		print '<p>There are no pages in the archive to operate on.</p>';
+	}
 }
 
 $nsisweb->end_page();
