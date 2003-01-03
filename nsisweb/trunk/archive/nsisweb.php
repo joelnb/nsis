e05fa368435593b2ed36bb94636f8f36a4e17777
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2003-01-03 02:55:21 +0000

    More valid HTML
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@325 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/nsisweb.php
+++ nsisweb/trunk/archive/nsisweb.php
@@ -38,25 +38,25 @@ The Archive currently contains <?= $pages_text ?> and has <?= $users_text ?>.
 </p>
 <ul>
   <li><b><a href="<?= $nsisweb->get_page_url('welcome') ?>">Welcome</a></b>
-  	<ul>General information about the NSIS Archive</ul>
+  	<p>General information about the NSIS Archive</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('contribute') ?>">Contribute</a></b>
-  	<ul>Help the archive grow by uploading your stuff to the archive</ul>
+  	<p>Help the archive grow by uploading your stuff to the archive</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('browse') ?>">Browse</a></b>
-    <ul>Dive right in and start browsing the archive for goodies!</ul>
+    <p>Dive right in and start browsing the archive for goodies!</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('download') ?>">Download</a></b>
-    <ul>Skip the information and head straight for the downloads</ul>
+    <p>Skip the information and head straight for the downloads</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('newest') ?>">Latest Additions/Changes</a></b>
-    <ul>See the latest additions and modifications</ul>
+    <p>See the latest additions and modifications</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('popular') ?>">Most Popular</a></b>
-    <ul>See which pages are visited most and which files are downloaded most frequently</ul>
+    <p>See which pages are visited most and which files are downloaded most frequently</p>
   </li>
   <li><b><a href="<?= $nsisweb->get_page_url('search') ?>">Search</a></b>
-    <ul>Need to find something or can't remember where you saw something before? Try this...</ul>
+    <p>Need to find something or can't remember where you saw something before? Try this...</p>
   </li>
 <?
 $session = $nsisweb->get_session();
@@ -64,7 +64,7 @@ if ($session->is_anonymous()) {
   $url = $nsisweb->get_page_url('createaccount');
   print <<<ENDOFHTML
     <li><b><a href="$url">Your Account</a></b>
-      <ul>Get your free account with us and get the credit for your contributions</ul>
+      <p>Get your free account with us and get the credit for your contributions</p>
     </li>
 ENDOFHTML;
 }
