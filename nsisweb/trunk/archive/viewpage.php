5a30a149adcbd4779291a8b8de6e1866cd545751
sunjammerx <sunjammerx@212acab6-be3b-0410-9dea-997c60f758d6>
2003-01-04 19:50:37 +0000

    Make showing a 404 error for a missing page work
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@359 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/archive/viewpage.php
+++ nsisweb/trunk/archive/viewpage.php
@@ -2,9 +2,11 @@
 include_once(dirname(__FILE__)."/engine/nsisweb.pkg.php");
 
 if($page = new NsisWebPage($_GET['pageid'],FETCH_CONTENT_PP)) {
-	$instance = new NsisWebInstance($page);
-	$instance->show();
-	exit;
+  if($page->is_okay()) {
+    $instance = new NsisWebInstance($page);
+    $instance->show();
+    exit;
+  }
 }
 
 $url = $nsisweb->get_base_url().'/templates/404.php?page='.urlencode($_GET['pageid']);
