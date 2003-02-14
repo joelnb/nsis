08e1e10ecf5fcbbcfcf4021b42a411e80101921c
rainwater <rainwater@212acab6-be3b-0410-9dea-997c60f758d6>
2003-02-14 18:47:15 +0000

    set content length
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@473 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/update.php
+++ nsisweb/trunk/update.php
@@ -2,6 +2,7 @@
 $latest = "1.98";
 $betaseries = "2.0";
 $betaversion = "b1";
+$output = "";
 
 if ($version) {
 	$update = false;
@@ -14,28 +15,29 @@ if ($version) {
         if ($latest>=$series) print "1|".$latest;
 		else if (!strcmp($betaseries,$series)) {
 			if (!strncmp($betaversion,$revision,1)) {
-				if (substr($betaversion,1)>substr($revision,1)||(substr($betaversion,1)==substr($revision,1)&&$cvs==1)) print "2|".$betaseries.$betaversion;
-				else print "0";
+				if (substr($betaversion,1)>substr($revision,1)||(substr($betaversion,1)==substr($revision,1)&&$cvs==1)) $output="2|".$betaseries.$betaversion;
+				else $output="0";
 			}
 			else if (!strncmp($betaversion,"b",1)&&!strncmp($revision,"a",1)) {
-				print "2|".$betaseries.$betaversion;
+				$output="2|".$betaseries.$betaversion;
 			}
-			else print "0";
+			else $output="0";
 		}
-		else print "0";
+		else $output="0";
 	}
 	else {
         if ($latest>$version) $update = true;
 		if ($update) {
-			print "1|".$latest;
+			$output="1|".$latest;
 		}
 		else {
-			print "0";
+			$output="0";
 		}
 	}
 }
 else {
-	print "0";
+	$output="0";
 }
+header("Content-Length: " . strlen($output));
+print $output;
 ?>
-
