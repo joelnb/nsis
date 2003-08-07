e848f72d81b8cc7c4918089e4b1b6fce2247e438
joostverburg <joostverburg@212acab6-be3b-0410-9dea-997c60f758d6>
2003-08-07 23:32:41 +0000

    redirect to new site
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@514 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/changelog.php
+++ nsisweb/trunk/changelog.php
@@ -1,3 +1,8 @@
+<?
+header("Location: /site/Changelog.24.0.html"); 
+exit();
+?>
+
 <? include("include/global.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
