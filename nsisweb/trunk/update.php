79fe536655f9c7de0139556326cadbccbe228901
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2005-08-26 17:46:48 +0000

    2.09
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@556 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/update.php
+++ nsisweb/trunk/update.php
@@ -28,8 +28,8 @@ $RTYPES = array(
 
 $output = "0";
 
-$latest_stable_version = "2.08";
-$latest_version = "2.08";
+$latest_stable_version = "2.09";
+$latest_version = "2.09";
 $latest_type = $TYPES[""];
 $latest_build = "";
 
