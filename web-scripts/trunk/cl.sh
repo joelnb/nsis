e292197088c5d60544593cda5f28bedb91860fb6
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2006-01-13 09:49:14 +0000

    timestamp from file and some code clean-up
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@4 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/cl.sh
+++ web-scripts/trunk/cl.sh
@@ -2,11 +2,25 @@
 
 # updates change log
 
+START_DATE=`find cl.timestamp -printf %Tc`
+
+PROJDIR=/home/groups/n/ns/nsis
+HTDOCSDIR=$PROJDIR/htdocs
+BINDIR=$PROJDIR/bin
+CVSDIR=$BINDIR/cvs/NSIS
+CVS2CL=$BINDIR/cvs2cl.pl
+USERMAP=$BINDIR/user.map
+CLHTMLNEW=$BINDIR/cl.htm
+CLHTML=$HTDOCSDIR/nightly/changelog.htm
+
 umask 0002
-cd /home/groups/n/ns/nsis/bin/cvs/NSIS
-../../cvsupdate.sh
-../../cvs2cl.pl -f "../changelog.txt" -l "-d'>Dec 17 15:59:23 UTC 2005'" -b -g "-q"
-if ! diff ../changelog.txt ../../../htdocs/nightly/changelog.htm; then
-	cp ../changelog.txt ../../../htdocs/nightly/changelog.htm
+
+cd $CVSDIR
+$BINDIR/cvsupdate.sh
+
+$CVS2CL -f $CLHTMLNEW -l "-d'>$START_DATE'" -b -g "-q"
+if ! diff $CLHTMLNEW $CLHTML; then
+	cp $CLHTMLNEW $CLHTML
 fi
-rm -f ../changelog.txt
+
+rm -f $CLHTMLNEW
