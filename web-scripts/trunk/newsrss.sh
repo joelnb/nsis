c75f5de1e9222ff2416768842c72ec81eebb20c2
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2006-01-24 19:31:58 +0000

    show last 3 items, just like on the project page
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@12 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/newsrss.sh
+++ web-scripts/trunk/newsrss.sh
@@ -2,5 +2,5 @@
 
 # downloads news feed and transforms it to html
 
-wget -q -O /home/groups/n/ns/nsis/news.rss "http://sourceforge.net/export/rss2_projnews.php?group_id=22049&rss_fulltext=1&rss_limit=2"
+wget -q -O /home/groups/n/ns/nsis/news.rss "http://sourceforge.net/export/rss2_projnews.php?group_id=22049&rss_fulltext=1&rss_limit=3"
 xsltproc /home/groups/n/ns/nsis/bin/newsrss.xsl /home/groups/n/ns/nsis/news.rss | tail -n 1 > /home/groups/n/ns/nsis/news.rss.html
