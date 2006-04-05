b9e2816945ecccb5cca90d497bebca74d137cbfa
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2006-04-05 18:57:52 +0000

    imported all scripts to cvs
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@15 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ web-scripts/trunk/forumrss-local.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# downloads forum feed and transforms it to html
+
+# this runs locally, and not on SourceForge shell sever.
+# SourceForge shell server doesn't allow outbound connections.
+# on SourceForge server side, it runs forumrss.sh
+
+wget -q -O /tmp/forum.rss "http://forums.winamp.com/backend.php?forumid=65&limit=5"
+if [ -e /tmp/forum.rss ]; then
+	cat /tmp/forum.rss | ssh -l kichik -i forumrss.key nsis.sf.net
+fi
+
+rm /tmp/forum.rss
+
+F=`mktemp`
+wget -q -O $F "http://nsis.sourceforge.net/Main_Page?action=purge"
+rm -f $F
