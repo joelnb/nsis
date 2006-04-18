ec3d7748a90291ceab081661564d663b9fb840c7
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2006-04-18 18:13:04 +0000

    some error checking
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@19 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/newsrss.sh
+++ web-scripts/trunk/newsrss.sh
@@ -2,5 +2,10 @@
 
 # downloads news feed and transforms it to html
 
-wget -q -O /home/groups/n/ns/nsis/news.rss "http://sourceforge.net/export/rss2_projnews.php?group_id=22049&rss_fulltext=1&rss_limit=3"
-xsltproc /home/groups/n/ns/nsis/bin/newsrss.xsl /home/groups/n/ns/nsis/news.rss | tail -n 1 > /home/groups/n/ns/nsis/news.rss.html
+if wget -q -O /home/groups/n/ns/nsis/news.rss "http://sourceforge.net/export/rss2_projnews.php?group_id=22049&rss_fulltext=1&rss_limit=3"; then
+	RT=`mktemp`
+	if xsltproc /home/groups/n/ns/nsis/bin/newsrss.xsl /home/groups/n/ns/nsis/news.rss > $RT; then
+		tail -n 1 $RT > /home/groups/n/ns/nsis/news.rss.html
+	fi
+	rm -f $RT
+fi
