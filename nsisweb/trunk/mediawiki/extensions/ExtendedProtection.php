fcc5473ef0edae693a8b3d747e76c5908d3bcc74
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2006-05-03 17:37:51 +0000

    tabs to spaces
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@570 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/mediawiki/extensions/ExtendedProtection.php
+++ nsisweb/trunk/mediawiki/extensions/ExtendedProtection.php
@@ -4,18 +4,18 @@ if (!defined('MEDIAWIKI')) die();
 $wgExtensionFunctions[] = 'wfExtendedProtection';
 
 function wfExtendedProtection() {
-	global $wgHooks;
+  global $wgHooks;
 
   $wgHooks['userCan'][] = 'wfExtendedProtectionUserCan';
 }
 
 function wfExtendedProtectionUserCan(&$title, &$user, $action, &$result) {
-	if ($title->getText() == 'EclipseNSIS - NSIS plugin for Eclipse') {
-		if ($user->isSysop() || $user->getName() == 'Iceman K')
-			return;
-		if ($action != 'read')
+  if ($title->getText() == 'EclipseNSIS - NSIS plugin for Eclipse') {
+    if ($user->isSysop() || $user->getName() == 'Iceman K')
+      return;
+    if ($action != 'read')
       $result = false;
-	}
+  }
 }
 
 ?>
