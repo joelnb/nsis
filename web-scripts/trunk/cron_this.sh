39a762205a6db0a86686523a4d381b1f714f2fba
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2007-08-24 11:43:45 +0000

    added sitemaps
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@30 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/cron_this.sh
+++ web-scripts/trunk/cron_this.sh
@@ -10,3 +10,5 @@ $scriptdir/nsisweb.sh > /dev/null 2>&1
 $scriptdir/newsrss.sh
 
 touch $scriptdir/cron_this.last_run
+
+php /home/groups/n/ns/nsis/htdocs/mediawiki/maintenance/generateSitemap.php nsis.sourceforge.net --fspath /home/groups/n/ns/nsis/htdocs/ > /dev/null 2>&1
