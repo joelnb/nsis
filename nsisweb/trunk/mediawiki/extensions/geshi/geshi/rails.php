e338e2850a8cafc910a9215eefe79790cf19117b
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2008-09-14 20:11:26 +0000

    geshi 1.0.8
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@5720 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/mediawiki/extensions/geshi/geshi/rails.php
+++ nsisweb/trunk/mediawiki/extensions/geshi/geshi/rails.php
@@ -4,10 +4,10 @@
  * ---------
  * Author: Moises Deniz
  * Copyright: (c) 2005 Moises Deniz
- * Release Version: 1.0.7.21
+ * Release Version: 1\.0\.8
  * Date Started: 2007/03/21
  *
- * Ruby language and Ruby on Rails Framework file for GeSHi
+ * Ruby (with Ruby on Rails Framework) language file for GeSHi.
  *
  *************************************************************************************
  *
@@ -360,7 +360,9 @@ $language_data = array (
     'URLS' => array(
         1 => '',
         2 => '',
-        3 => ''
+        3 => '',
+        4 => '',
+        5 => ''
         ),
     'OOLANG' => true,
     'OBJECT_SPLITTERS' => array(
@@ -381,7 +383,7 @@ $language_data = array (
             GESHI_BEFORE => '\\1',
             GESHI_AFTER => ''
             ),
-        2 => "([A-Z][a-zA-Z0-9_]*::)+[A-Z][a-zA-Z0-9_]*",
+        2 => "([A-Z][a-zA-Z0-9_]*::)+[A-Z][a-zA-Z0-9_]*", //Static OOP References
         3 => array(
             GESHI_SEARCH => "([[:space:]]|\[|\()(:[a-zA-Z_][a-zA-Z0-9_]*)",
             GESHI_REPLACE => '\\2',
