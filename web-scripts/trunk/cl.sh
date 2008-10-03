145a941b387b01ca67d5c79008b2defa0b23cd1e
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2008-10-03 10:45:06 +0000

    remove shell dependency from change log generation
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@5729 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/cl.sh
+++ web-scripts/trunk/cl.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
 
-# updates change log
+# updates change log and uploads
 
-PROJDIR=/home/groups/n/ns/nsis
-HTDOCSDIR=$PROJDIR/htdocs
-BINDIR=$PROJDIR/bin
-USERMAP=$BINDIR/user.map
-CLHTMLNEW=$BINDIR/cl.htm
+source config.sh
+
+BASEDIR=`dirname $0`
+
+# local
+
+XSL=$BASEDIR/cl.xsl
+LOGTMP=`mktemp`
+
+# remote
+
+HTDOCSDIR=$SFDIR/htdocs
 CLHTML=$HTDOCSDIR/nightly/changelog.htm
-
-XSL=$BINDIR/cl.xsl
-SVNLOG="svn log --xml --verbose"
 SVNROOT=https://nsis.svn.sourceforge.net/svnroot/nsis/NSIS/
 
-TAGREV=$((`cat $BINDIR/cl.tagrev` + 1))
+# others
 
-umask 0002
+SVNLOG="svn log --xml --verbose"
 
-if $SVNLOG -r HEAD:$TAGREV $SVNROOT | xsltproc $XSL - | grep -v '^<?xml version="1.0"?>$' > $CLHTMLNEW; then
-	if ! diff -q $CLHTMLNEW $CLHTML; then
-		cp $CLHTMLNEW $CLHTML
-	fi
+# latest tag
+
+TAG=`svn list $SVNROOT/tags | grep '^v' | cut -d / -f 1 | sort -n | tail -n 1`
+TAGINFO=`svn info --xml $SVNROOT/tags/$TAG`
+TAGREV=$((`echo $TAGINFO | xmlstarlet sel -t -m /info/entry/commit -v "@revision"` + 1))
+
+if $SVNLOG -r HEAD:$TAGREV $SVNROOT | xsltproc $XSL - | grep -v '^<?xml version="1.0"?>$' > $LOGTMP; then
+	scp -q -i $SFKEY $LOGTMP $SFUSER@$SFSERV:$CLHTML
 fi
 
-rm -f $CLHTMLNEW
+rm -f $LOGTMP
