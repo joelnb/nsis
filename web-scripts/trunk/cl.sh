b4cfe28c875f81ad9a7e015d1fb8c0adc3b4a8ab
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2008-11-08 16:19:54 +0000

    stop if tag info retrieval failed
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@5762 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/cl.sh
+++ web-scripts/trunk/cl.sh
@@ -9,7 +9,6 @@ source $BASEDIR/config.sh
 # local
 
 XSL=$BASEDIR/cl.xsl
-LOGTMP=`mktemp`
 
 # remote
 
@@ -22,9 +21,22 @@ SVNLOG="svn log --xml --verbose"
 
 # latest tag
 
-TAG=`svn list $SVNROOT/tags | grep '^v' | cut -d / -f 1 | sort -n | tail -n 1`
-TAGINFO=`svn info --xml $SVNROOT/tags/$TAG`
-TAGREV=$((`echo $TAGINFO | xmlstarlet sel -t -m /info/entry/commit -v "@revision"` + 1))
+if TAG=`svn list $SVNROOT/tags`; then
+	TAG=`echo $TAG | grep '^v' | cut -d / -f 1 | sort -n | tail -n 1`
+	if TAGINFO=`svn info --xml $SVNROOT/tags/$TAG`; then
+		TAGREV=$((`echo $TAGINFO | xmlstarlet sel -t -m /info/entry/commit -v "@revision"` + 1))
+	else
+		echo can't get tag info
+		exit 1
+	fi
+else
+	echo can't get tag list
+	exit 1
+fi
+
+# log it
+
+LOGTMP=`mktemp`
 
 if $SVNLOG -r HEAD:$TAGREV $SVNROOT | xsltproc $XSL - | grep -v '^<?xml version="1.0"?>$' > $LOGTMP; then
 	scp -q -i $SFKEY $LOGTMP $SFUSER@$SFSERV:$CLHTML
