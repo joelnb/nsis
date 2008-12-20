4d1c5851b5f37d06cae6686d09e98838af9abee6
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2008-12-20 16:18:12 +0000

    update for new mediawiki versions and new sourceforge configuration
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@5875 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/updatewiki.sh
+++ web-scripts/trunk/updatewiki.sh
@@ -3,74 +3,74 @@
 # updates mediawiki version
 #
 # usage example:
-#   updatewiki.sh 1.5.5
+#   updatewiki.sh 1.13 1.13.3
 # or, for local updates:
-#   updatewiki.sh 1.5.5 mediawiki-1.5.5.tar.gz
+#   updatewiki.sh 1.13 1.13.3 mediawiki-1.13.3.tar.gz
 
 cd /home/groups/n/ns/nsis/htdocs
 
-if [ -n "$2" ]; then
+if [ -n "$3" ]; then
 	if [ -f "$2" ]; then
-		tar xzf $2 || exit 1
-		rm -f $2
+		tar xzf $3 || exit 1
+		rm -f $3
 	else
-		echo "$2 cannot be found!"
+		echo "$3 cannot be found!"
 		exit 1
 	fi
 else
-	wget http://osdn.dl.sourceforge.net/sourceforge/wikipedia/mediawiki-$1.tar.gz || exit 1
-	tar xzf mediawiki-$1.tar.gz || exit 1
-	rm -f mediawiki-$1.tar.gz
+	wget http://download.wikimedia.org/mediawiki/$1/mediawiki-$2.tar.gz || exit 1
+	tar xzf mediawiki-$2.tar.gz || exit 1
+	rm -f mediawiki-$2.tar.gz
 fi
 
-rm -rf mediawiki-$1/config
+rm -rf mediawiki-$2/config
 
-cp -rf mediawiki/extensions/* mediawiki-$1/extensions
+cp -rf mediawiki/extensions/* mediawiki-$2/extensions
 
-cp -r mediawiki/skins/nsis mediawiki-$1/skins
-cp mediawiki/skins/NSIS.php mediawiki-$1/skins
+cp -r mediawiki/skins/nsis mediawiki-$2/skins
+cp mediawiki/skins/NSIS.php mediawiki-$2/skins
 
-cp mediawiki/.htaccess mediawiki-$1
+cp mediawiki/.htaccess mediawiki-$2
 
-cp mediawiki/spam_blacklist mediawiki-$1
+cp mediawiki/spam_blacklist mediawiki-$2
 
-cp mediawiki/LocalSettings.php mediawiki-$1
-cp mediawiki/AdminSettings.php mediawiki-$1
+cp mediawiki/LocalSettings.php mediawiki-$2
+cp mediawiki/AdminSettings.php mediawiki-$2
 
-sed -s "s/\([ \t]*NS_IMAGE[ \t]*=>[ \t]*'\)Image\(',\)/\1File\2/;s/\([ \t]*NS_IMAGE_TALK[ \t]*=>[ \t]*'\)Image\(_talk',\)/\1File\2/" mediawiki-$1/languages/Language.php > mediawiki-$1/languages/Language.php.sed
-mv -f mediawiki-$1/languages/Language.php.sed mediawiki-$1/languages/Language.php
+sed -s "s/\([ \t]*NS_IMAGE[ \t]*=>[ \t]*'\)Image\(',\)/\1File\2/;s/\([ \t]*NS_IMAGE_TALK[ \t]*=>[ \t]*'\)Image\(_talk',\)/\1File\2/" mediawiki-$2/languages/Language.php > mediawiki-$2/languages/Language.php.sed
+mv -f mediawiki-$2/languages/Language.php.sed mediawiki-$2/languages/Language.php
 
-chgrp -R nsis mediawiki-$1
-chmod -R g+w mediawiki-$1
+chgrp -R nsis mediawiki-$2
+chmod -R g+w mediawiki-$2
 
-rm -rf mediawiki-$1/images
-ln -s /tmp/persistent/nsiswikiimages mediawiki-$1/images
+rm -rf mediawiki-$2/images
+ln -s /home/groups/n/ns/nsis/persistent/mediawiki/nsiswikiimages mediawiki-$2/images
 
-diff -ruNw mediawiki mediawiki-$1 > wiki.diff
+diff -ruNw mediawiki mediawiki-$2 > wiki.diff
 
-sed -s "s/\/mediawiki/\/mediawiki-$1/" mediawiki-$1/LocalSettings.php > mediawiki-$1/LocalSettings.php.sed
-mv -f mediawiki-$1/LocalSettings.php.sed mediawiki-$1/LocalSettings.php
+sed -s "s/\/mediawiki/\/mediawiki-$2/" mediawiki-$2/LocalSettings.php > mediawiki-$2/LocalSettings.php.sed
+mv -f mediawiki-$2/LocalSettings.php.sed mediawiki-$2/LocalSettings.php
 
-echo "http://nsis.sourceforge.net/mediawiki-$1/index.php?title=Main_Page&action=purge"
-echo "http://nsis.sourceforge.net/mediawiki-$1/index.php?title=Change_Log&action=purge"
-echo "http://nsis.sourceforge.net/mediawiki-$1/index.php?title=Special:Version&action=purge"
-echo "http://nsis.sourceforge.net/mediawiki-$1/index.php?title=ExtractDLL&action=purge"
-echo "http://nsis.sourceforge.net/mediawiki-$1/index.php?title=File:Extractdll.zip&action=purge"
+echo "http://nsis.sourceforge.net/mediawiki-$2/index.php?title=Main_Page&action=purge"
+echo "http://nsis.sourceforge.net/mediawiki-$2/index.php?title=Change_Log&action=purge"
+echo "http://nsis.sourceforge.net/mediawiki-$2/index.php?title=Special:Version&action=purge"
+echo "http://nsis.sourceforge.net/mediawiki-$2/index.php?title=ExtractDLL&action=purge"
+echo "http://nsis.sourceforge.net/mediawiki-$2/index.php?title=File:Extractdll.zip&action=purge"
 echo
 echo "http://nsis.sourceforge.net/wiki.diff"
 
 read -n1 -p "Use new Wiki? [Y/n] " -e USE_NEW_WIKI
 
-sed -s "s/\/mediawiki-$1/\/mediawiki/" mediawiki-$1/LocalSettings.php > mediawiki-$1/LocalSettings.php.sed
-mv -f mediawiki-$1/LocalSettings.php.sed mediawiki-$1/LocalSettings.php
+sed -s "s/\/mediawiki-$2/\/mediawiki/" mediawiki-$2/LocalSettings.php > mediawiki-$2/LocalSettings.php.sed
+mv -f mediawiki-$2/LocalSettings.php.sed mediawiki-$2/LocalSettings.php
 
 if [ "$USE_NEW_WIKI" == '' -o "$USE_NEW_WIKI" == 'y' -o "$USE_NEW_WIKI" == 'Y' ]; then
-	mv mediawiki ../mediawiki-$1-old
-	mv mediawiki-$1 mediawiki
+	mv mediawiki ../mediawiki-$2-old
+	mv mediawiki-$2 mediawiki
 fi
 
 rm -f wiki.diff
 
 echo ""
 echo "Don't forget to delete the backup, once you're sure everything's working"
-echo "  /home/groups/n/ns/nsis/mediawiki-$1-old"
+echo "  /home/groups/n/ns/nsis/mediawiki-$2-old"
