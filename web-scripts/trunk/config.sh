36ba553b05c408d4458b4b2707bd75121980dad0
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2011-02-12 14:48:25 +0000

    new path
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@6134 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/config.sh
+++ web-scripts/trunk/config.sh
@@ -3,7 +3,7 @@
 SFSERV=web.sourceforge.net
 SFUSER=XXXX_USERNAME_XXXX,nsis
 SFKEY=XXXX_PATH_TO_PRIVATE_KEY_XXX
-SFDIR=/home/groups/n/ns/nsis
+SFDIR=/home/project-web/nsis
 
 SVNROOT=https://nsis.svn.sourceforge.net/svnroot/nsis/NSIS/
 
