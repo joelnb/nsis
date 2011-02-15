6dd317b858e624035119274e4035cb9424598bb9
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2011-02-15 09:14:54 +0000

    caused fatal error on newer versions of PHP
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@6136 212acab6-be3b-0410-9dea-997c60f758d6

--- nsisweb/trunk/mediawiki/skins/NSIS.php
+++ nsisweb/trunk/mediawiki/skins/NSIS.php
@@ -13,9 +13,6 @@
 if( !defined( 'MEDIAWIKI' ) )
         die( -1 );
 
-/** */
-require_once('includes/SkinTemplate.php');
-
 /**
  * Inherit main code from SkinTemplate, set the CSS and template filter.
  * @todo document
