620dbd01285c25691de3ee0a0bfe34a0f80d48b9
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2014-10-05 21:58:43 +0000

    enable test-code now that it works on linux
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@6546 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/nightly.sh
+++ web-scripts/trunk/nightly.sh
@@ -6,10 +6,10 @@
 #
 
 # get zlib library
-sudo apt-get -y install scons mingw32 mingw32-binutils zlib1g-dev
+sudo apt-get -y install scons mingw32 mingw32-binutils zlib1g-dev libcppunit-dev
 wget -O /tmp/zlib.zip "https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/zlib128-dll.zip?r=&ts=1403402496&use_mirror=hivelocity"
 mkdir /tmp/zlib
 unzip /tmp/zlib.zip -d /tmp/zlib
 
 # build
-scons ZLIB_W32=/tmp/zlib SKIPUTILS="NSIS Menu" dist-zip 2>&1 | tee build.log
+scons ZLIB_W32=/tmp/zlib SKIPUTILS="NSIS Menu" test dist-zip 2>&1 | tee build.log
