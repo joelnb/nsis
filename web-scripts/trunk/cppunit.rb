1b43291ccc1e0a397994145338075e93cda2ae67
anders_k <anders_k@212acab6-be3b-0410-9dea-997c60f758d6>
2017-06-03 02:56:46 +0000

    TravisCI OSX instance needs cppunit v1.13.2
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@6864 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ web-scripts/trunk/cppunit.rb
@@ -0,0 +1,21 @@
+# TravisCI OSX instance needs cppunit v1.13.2
+
+class Cppunit < Formula
+  desc "Unit testing framework for C++, forced version 1.13.2"
+  homepage "https://wiki.freedesktop.org/www/Software/cppunit/"
+  url "https://dev-www.libreoffice.org/src/cppunit-1.13.2.tar.gz"
+  sha256 "3f47d246e3346f2ba4d7c9e882db3ad9ebd3fcbd2e8b732f946e0e3eeb9f429f"
+
+  bottle do
+    cellar :any
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/DllPlugInTester", 2)
+  end
+end
