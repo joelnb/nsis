7b03a30017a2e9e011cf31d352d6a586a29cc649
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2020-03-17 21:34:16 +0000

    better unreleased background + https
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@7157 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/sflogoadder.sh
+++ web-scripts/trunk/sflogoadder.sh
@@ -5,6 +5,8 @@
 DOCS=$1/*.html
 
 for doc in $DOCS; do
-	sed -ie 's!</body>!<a href="http://sourceforge.net"><img border=0 src="http://sourceforge.net/sflogo.php?group_id=22049\&amp;type=1" alt="SourceForge Logo" /></a></body>!' $doc
-	sed -ie 's!<body>!<body><b style="color: red">Note: </b> this documentation is for the latest development version. Some commands may not yet be available in the released version.<hr />!' $doc
+	sed -ie 's!</body>!<a href="https://sourceforge.net"><img border=0 src="https://sourceforge.net/sflogo.php?group_id=22049\&amp;type=1" alt="SourceForge Logo" /></a></body>!' $doc
+	sed -ie 's!<body>!<body background="unreleased.png">!' $doc
 done
+
+cp unreleased.png $1
