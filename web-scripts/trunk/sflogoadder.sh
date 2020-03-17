110a14e64234ab46de3530001138f78807bb59ad
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2020-03-17 19:53:55 +0000

    add note about unreleased commands
    git-svn-id: svn://svn.code.sf.net/p/nsis/code@7156 212acab6-be3b-0410-9dea-997c60f758d6

--- web-scripts/trunk/sflogoadder.sh
+++ web-scripts/trunk/sflogoadder.sh
@@ -5,7 +5,6 @@
 DOCS=$1/*.html
 
 for doc in $DOCS; do
-	cat $doc | sed -e 's!</body>!<a href="http://sourceforge.net"><img border=0 src="http://sourceforge.net/sflogo.php?group_id=22049\&amp;type=1" alt="SourceForge Logo" /></a></body>!' > $doc.withlogo
-	rm $doc
-	mv $doc.withlogo $doc
+	sed -ie 's!</body>!<a href="http://sourceforge.net"><img border=0 src="http://sourceforge.net/sflogo.php?group_id=22049\&amp;type=1" alt="SourceForge Logo" /></a></body>!' $doc
+	sed -ie 's!<body>!<body><b style="color: red">Note: </b> this documentation is for the latest development version. Some commands may not yet be available in the released version.<hr />!' $doc
 done
